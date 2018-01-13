8fd3593b2322c4ecaf7fa3744d14b37c2743a41b
Sammi-Husky <sammi-husky@LIVE.COM>
2018-01-12 17:03:05 -0800

    add menu files for future menu UI

---
+++ source/menu.h
@@ -0,0 +1,18 @@
+#ifndef __MENU_H__
+#define __MENU_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Globals
+extern u8 menu_number;
+
+// Function prototypes
+void draw_menu();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __MENU_H__
